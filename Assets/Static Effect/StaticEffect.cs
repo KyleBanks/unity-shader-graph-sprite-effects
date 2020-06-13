ab4619849936679a14717e18d68455e52f51d9e7
Kyle Banks <k.banks@glofox.com>
2020-06-13 12:17:59 +0100

    Adding the Getting Started and Static Effect samples

---
+++ Assets/Static Effect/StaticEffect.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEffect : MonoBehaviour
+{
+
+    private static string MATERIAL_INTENSITY_PARAM = "_Intensity";
+
+    public float MaxIntensityDuration = 2f;
+    public float IntensityDecayDuration = 1f;
+
+    private Material _material;
+
+    private float _maxIntensityElapsed;
+    private float _intensityDecayElapsed;
+
+    void Awake()
+    {
+        this._material = this.GetComponent<SpriteRenderer>().material;
+    }
+
+    void Start() 
+    {
+        // Prevent the sprite from starting in a static phase by skipping to the end
+        // of the animation.
+        this._maxIntensityElapsed = MaxIntensityDuration;
+        this._intensityDecayElapsed = IntensityDecayDuration;    
+    }
+
+    void OnDestroy() 
+    {
+        // Clean up our material reference
+        Destroy(this._material);    
+    }
+
+    void Update()
+    {
+        // Restart the effect
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this._maxIntensityElapsed = 0f;
+            this._intensityDecayElapsed = 0f;
+        }
+        
+        // Calculate the intensity based on the phase of the animation
+        float intensity; 
+
+        // Max intensity phase
+        if (this._maxIntensityElapsed < MaxIntensityDuration)
+        {
+            this._maxIntensityElapsed += Time.deltaTime;
+            intensity = 1f;
+        }
+        // Decay phase
+        else if (this._intensityDecayElapsed < IntensityDecayDuration)
+        {
+            this._intensityDecayElapsed += Time.deltaTime;
+            intensity = Mathf.Lerp(1f, 0f, this._intensityDecayElapsed / IntensityDecayDuration);
+        }
+        // Effect Complete
+        else
+        {
+            intensity = 0f;
+        }
+
+        this._material.SetFloat(MATERIAL_INTENSITY_PARAM, intensity);
+    }
+}
