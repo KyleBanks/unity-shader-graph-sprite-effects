e15b60f8c12f609a555fab98b78ae9c17db903db
Kyle Banks <k.banks@glofox.com>
2020-06-14 11:22:12 +0100

    Adding space bar indicator for demo purposes

---
+++ Assets/StaticEffect/SpaceBarIndicator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceBarIndicator : MonoBehaviour
+{
+    
+    private CanvasGroup _canvas;
+
+    void Awake()
+    {
+        this._canvas = this.GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        this._canvas.alpha = Input.GetKey(KeyCode.Space) ? 1f : 0f;        
+    }
+}
